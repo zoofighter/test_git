ed064b11a2d255f7c867242fd8ee94368dc75311
zoofighter <zoofighter9@gmail.com>
2019-02-03 13:43:21 +0900

    sss

--- pandas/py.py
+++ pandas/py.py
@@ -1,1 +1,11 @@
-pypy
+http://www.sisain.co.kr/?mod=news&act=articleView&idxno=32487
+https://medium.com/@peteryun/ml-kaggle%EC%97%90-%EC%A0%81%EC%9A%A9%ED%95%B4%EB%B3%B4%EB%8A%94-xgboost-f1650342ba93
+https://blog.naver.com/gywlsangel/221345700705
+https://www.kaggle.com/c/instacart-market-basket-analysis/kernels
+https://www.0db.co.kr/xe/REVIEW_0DB/409449?ckattempt=2
+https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.dropna.html
+http://pinkwink.kr/1039
+https://stackoverflow.com/questions/36328029/is-there-a-hotkey-to-switch-between-split-window-panes
+https://stackoverflow.com/questions/17071871/select-rows-from-a-dataframe-based-on-values-in-a-column-in-pandas
+https://datascienceschool.net/view-notebook/f43be7d6515b48c0beb909826993c856/
+https://github.com/rasbt/python-machine-learning-book/blob/master/code/ch04/ch04.ipynb
